3664ef2b0e98f178f6e91307d7348a30dfa0ae06
Nguyen-Quang-Sang-Jizo <117348347+Nguyen-Quang-Sang-Jizo@users.noreply.github.com>
2024-07-26 16:37:59 +0700

    other_chart_scatter

--- MyReport.view.php
+++ MyReport.view.php
@@ -20,13 +20,13 @@
     $data2 = generateData();
 
     $datas = [
-        ["dataFirstX" => $data1[0]['x'], "dataFirstY" => $data1[0]['y'], "dataSecondX" => $data2[0]['x'], "dataSecondY" => $data2[0]['y']],
-        ["dataFirstX" => $data1[1]['x'], "dataFirstY" => $data1[1]['y'], "dataSecondX" => $data2[1]['x'], "dataSecondY" => $data2[1]['y']],
-        ["dataFirstX" => $data1[2]['x'], "dataFirstY" => $data1[2]['y'], "dataSecondX" => $data2[2]['x'], "dataSecondY" => $data2[2]['y']],
-        ["dataFirstX" => $data1[3]['x'], "dataFirstY" => $data1[3]['y'], "dataSecondX" => $data2[3]['x'], "dataSecondY" => $data2[3]['y']],
-        ["dataFirstX" => $data1[4]['x'], "dataFirstY" => $data1[4]['y'], "dataSecondX" => $data2[4]['x'], "dataSecondY" => $data2[4]['y']],
-        ["dataFirstX" => $data1[5]['x'], "dataFirstY" => $data1[5]['y'], "dataSecondX" => $data2[5]['x'], "dataSecondY" => $data2[5]['y']],
-        ["dataFirstX" => $data1[6]['x'], "dataFirstY" => $data1[6]['y'], "dataSecondX" => $data2[6]['x'], "dataSecondY" => $data2[6]['y']],
+        ["FirstX" => $data1[0]['x'], "FirstY" => $data1[0]['y'], "SecondX" => $data2[0]['x'], "SecondY" => $data2[0]['y']],
+        ["FirstX" => $data1[1]['x'], "FirstY" => $data1[1]['y'], "SecondX" => $data2[1]['x'], "SecondY" => $data2[1]['y']],
+        ["FirstX" => $data1[2]['x'], "FirstY" => $data1[2]['y'], "SecondX" => $data2[2]['x'], "SecondY" => $data2[2]['y']],
+        ["FirstX" => $data1[3]['x'], "FirstY" => $data1[3]['y'], "SecondX" => $data2[3]['x'], "SecondY" => $data2[3]['y']],
+        ["FirstX" => $data1[4]['x'], "FirstY" => $data1[4]['y'], "SecondX" => $data2[4]['x'], "SecondY" => $data2[4]['y']],
+        ["FirstX" => $data1[5]['x'], "FirstY" => $data1[5]['y'], "SecondX" => $data2[5]['x'], "SecondY" => $data2[5]['y']],
+        ["FirstX" => $data1[6]['x'], "FirstY" => $data1[6]['y'], "SecondX" => $data2[6]['x'], "SecondY" => $data2[6]['y']],
 
     ];
 
@@ -34,7 +34,7 @@
         'dataSource' => $datas,
         "series" => array(
             array(
-                "dataFirstX", "dataFirstY",
+                "FirstX", "FirstY",
                 array(
                     "label" => "My First dataset",
                     "backgroundColor" => 'rgba(255, 99, 132, 0.5)',
@@ -42,7 +42,7 @@
                 )
             ),
             array(
-                "dataSecondX", "dataSecondY",
+                "SecondX", "SecondY",
                 array(
                     "label" => "My Second dataset",
                     "backgroundColor" => 'rgba(54, 162, 235, 0.5)',
